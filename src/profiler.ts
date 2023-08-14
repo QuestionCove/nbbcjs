4893f04c8b4dce6db424896f2946960d1f3bb70e
James <ultrilliam@gmail.com>
2023-08-14 09:29:17 -0700

    Update profiler.ts
    Remove all PHP methods

--- src/profiler.ts
+++ src/profiler.ts
@@ -1,8 +1,3 @@
-import ksort from "locutus/php/array/ksort";
-import microtime from "locutus/php/datetime/microtime";
-import htmlspecialchars from "locutus/php/strings/htmlspecialchars";
-import sprintf from "locutus/php/strings/sprintf";
-
 export default class Profiler {
     start_time: Record<string, number>;
     total_times: Record<string, number>;
@@ -11,7 +6,7 @@ export default class Profiler {
         this.total_times = {};
     }
     public now() {
-        return microtime(true) - 1394060000;
+        return performance.now();
     }
     public begin(group: string) {
         this.start_time[group] = this.now();
@@ -31,11 +26,9 @@ export default class Profiler {
         return this.total_times[group];
     }
     public dumpAllGroups() {
-        console.log("<div>Profiled times:\n<ul>\n");
-        ksort(this.total_times);
+        console.log("Profiled times:");
         for (const [name, time] of Object.entries(this.total_times)) {
-            console.log("<li><b>"+htmlspecialchars(name)+"</b>: "+sprintf("%0.2f msec", time * 1000)+"</li>\n");
+            console.log(`    -${name}: ${time}ms`);
         }
-        console.log("</ul>\n</div>\n");
     }
 }
