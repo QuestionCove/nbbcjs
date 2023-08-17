ba616cfc042a9078d9a15c54f6d615c822182da4
James <ultrilliam@gmail.com>
2023-08-17 16:09:37 -0700

    Add escape character testing

--- tests/nbbc.test.ts
+++ tests/nbbc.test.ts
@@ -838,6 +838,18 @@ const tests = {
             html: "<div style=\"border: 1px solid red\">bordered text</div>"
         }
     ],
+    "Escaped Character Tests": [
+        {
+            descr: "Basic Escaped functionality works",
+            bbcode: "the \\[b] tag should not be parsed",
+            html: "the [b] tag should not be parsed"
+        },
+        {
+            descr: "Multitag tags like [list] should not be parsed when escaped",
+            bbcode: "\\[list] [*] test1 [*] test2 \\[list] [*] test 1 [/list]",
+            html: "[list] [*] test1 [*] test2 [list] [*] test 1 [/list]"
+        }
+    ],
     "Misc Tests": [
         {
             descr: "Complex document test",
